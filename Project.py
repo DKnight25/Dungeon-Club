1577cdbf5f892d44172b8d4784c48eac03baef53
drozdovar <102758617+drozdovar@users.noreply.github.com>
2022-04-22 06:17:45 +0300

    Several bugfixes

--- Project.py
+++ Project.py
@@ -1,29 +1,46 @@
 import pickle
-class Sold(): #Soldier -- Солдат
-    sh_fn=None #Sh -- Сокращено (shortened)    First name -- Имя
-    sh_ptnc=None #ptnc -- Отчество (Patronic), у англоговорящих нет в повседневных словах полного аналога, но есть это...
-    lname=None #Last name -- Фамилия
-    titl=None # Title -- Звание [A,К,Хамар,СтаршийСержант,2003,5700]
-    yr=None # Year -- Год (Начания... Начинания... Вступления в службу)
-    wg=None # Wage -- Оплата (Оклад)
-file_input = open ('base.txt','r',encoding="utf8")
-slds=[] # Soldiers -- Солдаты
-while True:
-    x=file_input.readline()
-    if not x:break
-    x=x.split()
-    sd=Sold()
-    sd.sh_fn=x[1]
-    sd.sh_ptnc=x[2]
-    lname=x[0]
-    sd.titl=x[3]
-    sd.yr=x[4]
-    sd.wg=x[5]
-    slds.append(sd)
+class Sold():
+    sh_fn=None
+    sh_ptnc=None
+    lname=None
+    titl=None
+    yr=None 
+    wg=None 
+slds_tmp=[]
+with open('base.dat','rb') as file_input:
+    slds_tmp=pickle.load(file_input)
+    file_input.close()
+slds=[[] * i for i in range (len(slds_tmp))]
+for i in range (len(slds_tmp)):
+    slds[i].append(slds_tmp[i].sh_fn)
+    slds[i].append(slds_tmp[i].sh_ptnc)
+    slds[i].append(slds_tmp[i].lname)
+    slds[i].append(slds_tmp[i].titl)
+    slds[i].append(slds_tmp[i].yr)
+    slds[i].append(slds_tmp[i].wg)
 def Exit():
+    print ('chego')
     exit()
 def sortfog():
-    com=input()
+    while True:
+        try:
+            com=int(input())
+            k=0
+            r=0
+            p=[]
+            for i in range(len(slds)):
+                for j in range(k,len(slds)):
+                    if slds[j][com]<slds[r][com]:
+                        r=j
+                p=slds[k]
+                slds[k]=slds[r]
+                slds[r]=p
+                k+=1
+                r=k
+                print(slds)
+            break
+        except:
+            print ('An exeption occured in sortfog')
 def append():
     while True:
         com=list(input().split())
@@ -31,10 +48,9 @@ def append():
             slds.append([com[:6]])
             break
         except:
-            pass
+            print ('An exeption occured in append')
 def edit():
     com=input()
-    
 def delete():
     while True:
         com=int(input())
@@ -42,45 +58,44 @@ def delete():
             slds.pop(com)
             break
         except:
-            pass
+            print ('An exeption occured in delete')
 def burn():
-    pass
+    slds_end=[]
+    for i in range (len(slds)):
+        sd=Sold()
+        sd.sh_fn=slds[i][0]
+        sd.sh_ptnc=slds[i][1]
+        sd.lname=slds[i][2]
+        sd.titl=slds[i][3]
+        sd.yr=slds[i][4]
+        sd.wg=slds[i][5]
+        slds_end.append(sd)
+with open('base.dat','wb') as file_output:
+    pickle.dump(slds,file_output)
+    file_output.close()
 def print_soldier():
-    com=input()
-    for i in range(len(slds)):
-        if slds[i][2]==com:
-            print(i,end=')')
-            for j in range(6):
-                print(slds[i][j],end=' ')
-            print('')
-            break
+    com=int(input())
+    for i in range(6):
+        print(slds[com][i],end=' ')
+    print('')
 def print_all():
     for i in range(len(slds)):
+        print(i,end=') ')
         for j in range(6):
             print(slds[i][j],end=' ')
         print('')
-metod={
-    'exit':Exit
-    'sort':sortfog
-    'append':append
-    'edit':edit
-    'delete':delete
-    'burn':burn
-    'print soldier':print_soldier
+metod={'exit':Exit,
+    'sort':sortfog,
+    'append':append,
+    'edit':edit,
+    'delete':delete,
+    'burn':burn,
+    'print soldier':print_soldier,
     'print all':print_all
     }
 while True:
     com=input()
-    if com=='exit':
-        break
-    if com=='sort':
-        com=input()
-        if com=='lname':
-            pass
-        if com=='wage':
-            pass
-        if com=='year':
-            pass
-    if com=='burn':
-        #Запись в файл под тем же или новым именем
-        pass
+    try:
+        metod[com]()
+    except:
+        print ('An exeption occured in execution')
